f2402e0dd1adda7809f44e90b08ff0c278caba09
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-03-23 18:19:01 +0800

    feat: add terser npm

--- dist/tools.js
+++ dist/tools.js
@@ -1,17 +1,1 @@
-var tools = (function (exports) {
-  'use strict';
-
-  const square = (num) => {
-    return num * num;
-  };
-
-  const cube = (num) => {
-    return num * num * num;
-  };
-
-  exports.cube = cube;
-  exports.square = square;
-
-  return exports;
-
-})({});
+var tools=function(r){"use strict";return r.cube=r=>r*r*r,r.square=r=>r*r,r}({});
